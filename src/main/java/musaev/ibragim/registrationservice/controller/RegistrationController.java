aaf80b9269831346ce0c42f789c2080b8cdaf437
Ibragimovic <m.ibragim88@gmail.com>
2023-10-24 18:29:16 +0600

    created AuthenticationController with authenticateUser method

--- src/main/java/musaev/ibragim/registrationservice/controller/RegistrationController.java
+++ src/main/java/musaev/ibragim/registrationservice/controller/RegistrationController.java
@@ -1,5 +1,7 @@
 package musaev.ibragim.registrationservice.controller;
 
+import musaev.ibragim.registrationservice.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
---
+++ src/main/java/musaev/ibragim/registrationservice/service/UserService.java
@@ -0,0 +1,7 @@
+package musaev.ibragim.registrationservice.service;
+
+public class UserService {
+    public void registerUser(UserDTO userDTO) {
+
+    }
+}

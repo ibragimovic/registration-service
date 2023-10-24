221245024d8bd4e1a1e5938bfa2c0e217d05faa8
Ibragimovic <m.ibragim88@gmail.com>
2023-10-24 23:40:20 +0600

    created CustomUserDetailsService with loadUserByUsername method

---
+++ src/main/java/musaev/ibragim/registrationservice/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package musaev.ibragim.registrationservice.service;
+
+import musaev.ibragim.registrationservice.entity.User;
+import musaev.ibragim.registrationservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService  implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with this email: " + email);
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(email)
+                .password(user.getPassword())
+                .authorities("ROLE_USER")
+                .accountExpired(!user.isAccountNonExpired())
+                .accountLocked(!user.isAccountNonLocked())
+                .credentialsExpired(!user.isCredentialsNonExpired())
+                .disabled(!user.isEnabled())
+                .build();
+    }
+
+}

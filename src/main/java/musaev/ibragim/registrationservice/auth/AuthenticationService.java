b83142005fee847240b910e99340c353675c9e81
Ibragimovic <m.ibragim88@gmail.com>
2023-10-27 14:18:20 +0600

    created AuthenticationService

---
+++ src/main/java/musaev/ibragim/registrationservice/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package musaev.ibragim.registrationservice.auth;
+
+import lombok.RequiredArgsConstructor;
+import musaev.ibragim.registrationservice.config.JwtService;
+import musaev.ibragim.registrationservice.user.Role;
+import musaev.ibragim.registrationservice.user.User;
+import musaev.ibragim.registrationservice.user.UserRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(@NotNull RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    request.getEmail(),
+                    request.getPassword()
+                )
+        );
+        //TODO Here I need to specify which Exception I will throw and where I will catch it
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}

262a06ccbb24831713a66ef9109a5747f65e8309
Ibragimovic <m.ibragim88@gmail.com>
2023-11-15 15:35:38 +0600

    added feature for saving tokens in AuthenticationService

--- src/main/java/musaev/ibragim/registrationservice/auth/AuthenticationService.java
+++ src/main/java/musaev/ibragim/registrationservice/auth/AuthenticationService.java
@@ -2,6 +2,9 @@ package musaev.ibragim.registrationservice.auth;
 
 import lombok.RequiredArgsConstructor;
 import musaev.ibragim.registrationservice.config.JwtService;
+import musaev.ibragim.registrationservice.token.Token;
+import musaev.ibragim.registrationservice.token.TokenRepository;
+import musaev.ibragim.registrationservice.token.TokenType;
 import musaev.ibragim.registrationservice.user.Role;
 import musaev.ibragim.registrationservice.user.User;
 import musaev.ibragim.registrationservice.user.UserRepository;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -29,8 +33,9 @@ public class AuthenticationService {
                 .role(Role.USER)
                 .build();
 
-        userRepository.save(user);
+        var savedUser = userRepository.save(user);
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(savedUser, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
@@ -46,8 +51,33 @@ public class AuthenticationService {
         //TODO Here I need to specify which Exception I will throw and where I will catch it
         var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokensByUser(user.getId());
+        if (validUserTokens.isEmpty()) {
+            return;
+        }
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .revoked(false)
+                .expired(false)
+                .build();
+        tokenRepository.save(token);
     }
 }

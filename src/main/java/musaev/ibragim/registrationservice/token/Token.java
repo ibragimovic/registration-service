21dff2226bf44878c29795eeef9ae16c993799a4
Ibragimovic <m.ibragim88@gmail.com>
2023-11-15 15:29:57 +0600

    created Token entity

---
+++ src/main/java/musaev/ibragim/registrationservice/token/Token.java
@@ -0,0 +1,33 @@
+package musaev.ibragim.registrationservice.token;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import musaev.ibragim.registrationservice.user.User;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+
+    private boolean expired;
+
+    private boolean revoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}

fa2082d4a0b549d5c128089efd404541d2f13ae6
Ibragimovic <m.ibragim88@gmail.com>
2023-11-24 16:55:52 +0600

    updated RegistrationServiceApplication, added test users

--- src/main/java/musaev/ibragim/registrationservice/RegistrationServiceApplication.java
+++ src/main/java/musaev/ibragim/registrationservice/RegistrationServiceApplication.java
@@ -1,13 +1,44 @@
 package musaev.ibragim.registrationservice;
 
+import musaev.ibragim.registrationservice.auth.AuthenticationService;
+import musaev.ibragim.registrationservice.auth.RegisterRequest;
+import musaev.ibragim.registrationservice.user.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static musaev.ibragim.registrationservice.user.Role.ADMIN;
+import static musaev.ibragim.registrationservice.user.Role.MANAGER;
 
 @SpringBootApplication
 public class RegistrationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AuthenticationService service) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Manager")
+					.lastname("Manager")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+		};
 	}
 
 }
